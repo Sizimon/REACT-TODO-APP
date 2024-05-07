5e01538b8ce2e94046896784e2e482bac3218493
Szymon Samus <siizmonsus@gmail.com>
2024-05-07 17:37:59 +0200

    Add: Styling and base for extra functionality to add catagories to todoItem objects through the editing screen.

--- todo-app/src/components/TodoItem.jsx
+++ todo-app/src/components/TodoItem.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useRef, useState } from "react";
+import { ColorPicker, useColor } from "react-color-palette";
+import "react-color-palette/css";
 
 export default function TodoItem({ todo, editTodo, editingItemId, editDescription }) {
     const dialogRef = useRef(null);
     const [description, setDescription] = useState(todo.description);
+    const [color, setColor] = useColor("hex", "#00FF00");
 
     useEffect(() => {
         if (todo.isEditing && todo.id === editingItemId) {
@@ -51,26 +54,46 @@ export default function TodoItem({ todo, editTodo, editingItemId, editDescriptio
             {todo.isEditing && todo.id === editingItemId && (
                 <dialog
                     ref={dialogRef}
-                    className="grid grid-cols-4 md:grid-cols-3 p-6 rounded-lg bg-slate-900 text-white">
-                    <div className="col-span-4 md:col-span-2 text-center">
-                        <h2 className="uppercase">{todo.task}</h2>
+                    className="grid grid-cols-3 md:grid-cols-3 p-6 rounded-lg border border-white bg-slate-900 text-white">
+                    <div className="col-span-3 md:col-span-2 text-center">
+                        <h1 className="uppercase pb-6 text-2xl">{todo.task}</h1>
                         <textarea
                             value={description}
                             onChange={e => setDescription(e.target.value)}
-                            cols="60"
-                            rows="14"
+                            cols="52"
+                            rows="12"
                             className="bg-slate-900 border border-white rounded-lg p-2 text-white text-center"
                         />
+                        <div className="flex flex-col justify-center m-auto my-6">
+                            <ul className="list-none p-12 border border-white rounded-lg">
+                                SELECTED CATAGORIES
+                            </ul>
+                        </div>
                         <div className="flex flex-row justify-center gap-36 m-auto">
-                            <button>Woopsie</button>
-                            <button onClick={() => {
-                                closeDialog(todo.id);
-                                editDescription(todo.id, description);
-                            }}>Save & Close</button>
+                            <button className="bg-white text-slate-900 rounded-lg p-2">Woopsie</button>
+                            <button
+                                className="bg-white text-slate-900 rounded-lg p-2"
+                                onClick={() => {
+                                    closeDialog(todo.id);
+                                    editDescription(todo.id, description);
+                                }}>Save & Close</button>
                         </div>
                     </div>
-                    <div>
-                        WILL BE CATAGORY SECTION
+                    <div className="col-span-3 md:col-span-1 text-center">
+                        <h2 className="uppercase pb-6 text-md">Add Catagories</h2>
+                        <form className="flex flex-col m-2 gap-2">
+                            <label>Catagory Name:</label>
+                            <input className="bg-slate-900 border border-white rounded-lg text-white text-center" type="text" />
+                            <h2>Color</h2>
+                            <ColorPicker
+                                width={456}
+                                height={100}
+                                color={color}
+                                onChange={setColor}
+                                hideHSV
+                                dark />
+                            <button className="bg-white text-slate-900 rounded-lg p-2">Add as Catagory</button>
+                        </form>
                     </div>
                 </dialog>
             )}
