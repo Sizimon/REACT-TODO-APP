e7ed8d0b63f05d3bfc8ad22ca06d10f1624a3ada
Szymon Samus <siizmonsus@gmail.com>
2024-05-19 21:56:43 +0200

    Feat: Fix Footer styling

--- todo-app/src/components/Footer.jsx
+++ todo-app/src/components/Footer.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="flex flex-col justify-center items-center bg-gray-800 p-4 text-white sticky w-full z-10 bottom-0">You are logged in as, ...</div>
+    <div className="flex flex-col justify-center items-center bg-gray-800 p-4 text-white sticky w-full z-10 bottom-0 mt-auto">You are logged in as, ...</div>
   )
 }
