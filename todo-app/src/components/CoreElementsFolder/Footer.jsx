70d6d077d42f87d3ac7b1f315044513b94cfccd1
Shhmon <Siizmonsus@gmail.com>
2024-06-19 18:12:29 +0200

    Fix: Fix overlapping timers and sticky footer

--- todo-app/src/components/CoreElementsFolder/Footer.jsx
+++ todo-app/src/components/CoreElementsFolder/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer({todos}) {
   const priorityTaskAmount = todos.filter(todo => todo.priority).length
 
   return (
-    <div className="flex flex-col justify-center items-center bg-zinc-800 border-t border-zinc-400 p-4 text-white sticky w-full z-10 bottom-0 mt-auto">
+    <div className="flex flex-col justify-center items-center bg-zinc-800 border-t border-zinc-400 p-4 text-white w-full z-10 mt-auto">
       <p>Welcome back USER!</p>
       <p>You have {taskAmount} tasks to complete. Of which 3 are due today.</p>
       <p>You have {priorityTaskAmount} task set as a priority, click here to see which!</p>
